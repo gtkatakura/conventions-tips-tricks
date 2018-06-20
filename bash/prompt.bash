983e2fa1001c6f2444e6aaf5a83510c7269a8c01
gtkatakura <gt.katakura@gmail.com>
2018-06-20 09:12:17 -0300

    feat(bash): prompt

---
+++ bash/prompt.bash
@@ -0,0 +1,12 @@
+COLOR_OFF="\[\033[0m\]"
+GREEN="\[\033[0;32m\]"
+YELLOW="\[\033[0;33m\]"
+
+function parse_git_branch {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
+}
+
+export PS1="${GREEN}\w ${YELLOW}\$(parse_git_branch)${COLOR_OFF}$ "
+# export PS1="${GREEN}\w ${YELLOW}\$(parse_git_branch)${COLOR_OFF}\nλ "
+# export PS1="${GREEN}\w ${COLOR_OFF}$ "
+# export PS1="${YELLOW}$(parse_git_branch)${COLOR_OFF}$ "
