8db1a94fb1ca24452e020b5f0d2e35303180ada8
gtkatakura <gt.katakura@gmail.com>
2019-10-28 12:00:59 -0300

    feat: vscode install from microsoft apt repository

--- bash/snap-aliases.bash
+++
@@ -1,1 +0,0 @@
-alias code='snap run code'
---
+++ ubuntu/vscode-install.bash
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget -y
+
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+sudo add-apt-repository 'deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main'
+
+sudo apt update
+sudo apt install code
