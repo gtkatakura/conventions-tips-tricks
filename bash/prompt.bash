93dbdc551cd2759d093fa5c35a831683901160a9
gtkatakura <gt.katakura@gmail.com>
2020-04-17 17:28:37 -0300

    feat: add vtex io info to prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -6,7 +6,27 @@ function parse_git_branch {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
 }
 
-export PS1="${GREEN}\w ${YELLOW}\$(parse_git_branch)${COLOR_OFF}$ "
+function get_vtex_io_info {
+  function get_vtex_json {
+    cat $HOME/.config/configstore/vtex.json
+  }
+
+  function get_vtex_account {
+    get_vtex_json | jq .account -r
+  }
+
+  function get_vtex_workspace {
+    get_vtex_json | jq .workspace -r
+  }
+
+  if [ -d .git ]; then
+    if [[ $(git remote -v) =~ "github.com/vtex" ]]; then
+      echo "[$(get_vtex_account)/$(get_vtex_workspace)] "
+    fi
+  fi
+}
+
+export PS1="${GREEN}\w ${BLUE}\$(get_vtex_io_info)${YELLOW}\$(parse_git_branch)${COLOR_OFF}$ "
 # export PS1="${GREEN}\w ${YELLOW}\$(parse_git_branch)${COLOR_OFF}\nλ "
 # export PS1="${GREEN}\w ${COLOR_OFF}$ "
 # export PS1="${YELLOW}$(parse_git_branch)${COLOR_OFF}$ "
