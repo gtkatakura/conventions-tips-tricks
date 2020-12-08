3f492b7c18b8ee9e8fa75c97117dd930831341f9
gtkatakura <gt.katakura@gmail.com>
2020-12-08 11:46:54 -0300

    fix: update prerequisites for asdf-ruby

--- ubuntu/asdf/ruby-install.bash
+++ ubuntu/asdf/ruby-install.bash
@@ -1,5 +1,5 @@
 green_text "Installing prerequisites for asdf-ruby"
-sudo apt install build-essential libssl-dev libreadline-dev zlib1g-dev -y
+sudo apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev libdb-dev
 
 green_text "Adding ruby to asdf"
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
