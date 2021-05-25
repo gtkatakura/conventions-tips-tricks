bf514d54f9c1dd2b33b0622c91fac8731cbb2eac
gtkatakura <gt.katakura@gmail.com>
2021-05-25 10:37:29 -0300

    feat: discord-install using deb

--- ubuntu/snap/discord-install.bash
+++ ubuntu/snap/discord-install.bash
@@ -1,2 +1,5 @@
-green_text "Installing Discord (using snap)"
-sudo snap install discord
+green_text "Downloading Discord (deb)"
+sudo curl -L 'https://discord.com/api/download?platform=linux&format=deb' -o /tmp/discord.deb
+
+green_text "Installing Discord"
+sudo apt install /tmp/discord.deb -y
