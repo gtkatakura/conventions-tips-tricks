15f354423bb4c0cb06fbe31d820a9ba1774b6dbb
gtkatakura <gt.katakura@gmail.com>
2021-08-20 15:57:42 -0300

    feat: add openvpn install

---
+++ ubuntu/apt/openvpn-install.bash
@@ -0,0 +1,8 @@
+green_text "Installing openvpn (using apt)"
+sudo apt install openvpn -y
+
+green_text "Installing network-manager-openvpn (using apt)"
+sudo apt install network-manager-openvpn -y
+
+green_text "Installing network-manager-openvpn-gnome (using apt)"
+sudo apt install network-manager-openvpn-gnome -y
