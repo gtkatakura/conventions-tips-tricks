2de92835d10615e55b130ee8f9d4c2aa44eac73e
gtkatakura <gt.katakura@gmail.com>
2021-09-22 14:17:30 -0300

    fix(apt): add -y flag to some installs

--- ubuntu/apt/ssh-install.bash
+++ ubuntu/apt/ssh-install.bash
@@ -1,5 +1,5 @@
 green_text "Installing openssh-server (using apt)"
-sudo apt install openssh-server
+sudo apt install openssh-server -y
 
 blue_text "Disabling ssh server (systemctl disable ssh)"
 sudo systemctl disable ssh
