eb5293091959487d32af1634fce3f1a363e999c3
gtkatakura <gt.katakura@gmail.com>
2022-07-08 16:12:37 -0300

    feat(asdf): updates version to 0.10.0

--- ubuntu/manual/asdf-install.bash
+++ ubuntu/manual/asdf-install.bash
@@ -1,5 +1,5 @@
 green_text "Downloading asdf"
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.1
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
 
 green_text "Adding asdf bash files to ~/.bashrc"
 echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
