d2b05401393bbc81ce051febd39ae934216ea884
gtkatakura <gt.katakura@gmail.com>
2022-10-08 01:55:14 -0300

    feat(asdf): elixir install

---
+++ ubuntu/asdf/elixir-install.bash
@@ -0,0 +1,5 @@
+green_text "Adding erlang to asdf"
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+
+green_text "Adding elixir to asdf"
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
