3b7f16bc9eb373d25f4bb1a5ba2b25dc5d897c64
gtkatakura <gt.katakura@gmail.com>
2022-10-25 20:34:15 -0300

    fix(gh-cli): updates GPG signing key

--- ubuntu/apt/gh-install.bash
+++ ubuntu/apt/gh-install.bash
@@ -1,7 +1,7 @@
 # ref: https://github.com/cli/cli/blob/trunk/docs/install_linux.md
 
 blue_text "Adding https://cli.github.com/packages to apt repositories"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 23F3D4EA75716059
 sudo add-apt-repository https://cli.github.com/packages -y
 sudo apt update
 
