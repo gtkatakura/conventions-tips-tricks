12bca06a30238bed151d170e4856213b33e2955c
gtkatakura <gt.katakura@gmail.com>
2022-11-03 06:40:45 -0300

    fix(ubuntu): gnome configurations

---
+++ ubuntu/configure.bash
@@ -0,0 +1,10 @@
+# theme-install tem a base
+
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position "RIGHT"
+
+gnome-shell-extension-tool -e "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
+gnome-shell-extension-tool -e "places-menu@gnome-shell-extensions.gcampax.github.com"
+
+# cat /proc/sys/fs/inotify/max_user_watches
+# sudo sysctl fs.inotify.max_user_watches=582222 && sudo sysctl -p
