2e6f17b99665721852da257b90d873befe6a1d8d
gtkatakura <gt.katakura@gmail.com>
2023-05-23 18:00:20 -0300

    chore(ubuntu): support to args at run-emulator

--- bin/run-emulator.bash
+++ bin/run-emulator.bash
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 PS3='Please enter your choice: '
-devices=($($ANDROID_HOME/emulator/emulator -list-avds))
+devices=$("$ANDROID_HOME"/emulator/emulator -list-avds)
+option=$1
+
+if [ -n "$option" ]; then
+  device=${devices[$option - 1]}
+
+  if [ -n "$device" ]; then
+    echo "Opening the device: $device"
+    "$ANDROID_HOME"/emulator/emulator -avd "$device" &> /dev/null &
+    exit
+  fi
+fi
 
 select device in "${devices[@]}"
 do
-  if [ -z "$device" ]
-  then
+  if [ -z "$device" ]; then
     echo "Invalid option $REPLY"
   else
-    $ANDROID_HOME/emulator/emulator -avd $device &> /dev/null &
+    echo "Opening the device: $device"
+    "$ANDROID_HOME"/emulator/emulator -avd "$device" &> /dev/null &
     exit
   fi
 done
