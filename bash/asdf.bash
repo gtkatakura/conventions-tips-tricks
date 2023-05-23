1b706c6f58fa5bbc22fd5e91d24c4e953b3ce5ee
gtkatakura <gt.katakura@gmail.com>
2023-05-23 18:03:21 -0300

    feat(ubuntu): java install

---
+++ bash/asdf.bash
@@ -0,0 +1,3 @@
+if [ -f "$HOME/.asdf/plugins/java/set-java-home.bash" ]; then
+  source "$HOME"/.asdf/plugins/java/set-java-home.bash
+fi
---
+++ ubuntu/asdf/java-install.bash
@@ -0,0 +1,2 @@
+green_text "Adding java to asdf"
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
