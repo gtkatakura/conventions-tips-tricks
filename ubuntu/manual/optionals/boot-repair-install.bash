b5f9e1c90adb7d4ce00a8879aea49e257ce7db83
gtkatakura <gt.katakura@gmail.com>
2023-05-23 15:14:01 -0300

    chore(ubuntu): boot-repair

---
+++ ubuntu/manual/optionals/boot-repair-install.bash
@@ -0,0 +1,10 @@
+
+sudo add-apt-repository ppa:yannubuntu/boot-repair && sudo apt-get update
+sudo apt-get install -y boot-repair && boot-repair
+
+# LegacyWindows detected. Please enable BIOS-compatibility/CSM/Legacy mode in your UEFI firmware, and use this software from a live-CD (or live-USB).
+
+# https://youtu.be/XvAMe59YF6k
+# How to Fix Legacy Windows Detected the Boot of Your Pc is in UEFI Mode Boot Repair [2021-Guide]
+
+# https://forums.linuxmint.com/viewtopic.php?t=335230
