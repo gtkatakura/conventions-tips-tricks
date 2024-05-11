788a6fdad0dc1ab549561754158dd8af57bb6c14
gtkatakura <gt.katakura@gmail.com>
2024-05-11 17:23:31 -0300

    feat(bash): awslocal alias

---
+++ bash/aws-aliases.bash
@@ -0,0 +1,1 @@
+alias awslocal="AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION} aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566"
