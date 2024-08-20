a5c182c757bbe8183e0905574c0187cbdbef1507
gtkatakura <gt.katakura@gmail.com>
2024-08-20 16:28:35 -0300

    feat(ubuntu): vysor install

---
+++ ubuntu/apt/vysor-install.bash
@@ -0,0 +1,8 @@
+blue_text "Setting up the repository (vysor)"
+echo \
+  "deb [arch=$(dpkg --print-architecture), trusted=yes] https://nuts.vysor.io/apt ./" \
+  | sudo tee /etc/apt/sources.list.d/vysor.list > /dev/null
+
+green_text "Installing vysor"
+sudo apt-get update
+sudo apt-get install vysor -y
