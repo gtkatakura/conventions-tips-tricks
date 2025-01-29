49b01dccc767418ff10e4bcd558b00f96312e641
gtkatakura <gt.katakura@gmail.com>
2025-01-29 15:00:59 -0300

    feat(ubuntu): use numpad keys for switch to workspace shortcut

--- ubuntu-24_04/install/desktop/set-gnome-extensions.sh
+++ ubuntu-24_04/install/desktop/set-gnome-extensions.sh
@@ -11,6 +11,15 @@ gsettings set org.gnome.shell.extensions.tactile col-2 1
 gsettings set org.gnome.shell.extensions.tactile col-3 1
 gsettings set org.gnome.shell.extensions.tactile gap-size 0
 
+# Use super for workspaces
+# Preference for the numpad keys
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>KP_1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>KP_2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>KP_3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>KP_4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>KP_5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>KP_6']"
+
 # Configure TopHat
 # gsettings list-keys org.gnome.shell.extensions.tophat
 gsettings set org.gnome.shell.extensions.tophat show-icons true
@@ -21,3 +30,6 @@ gsettings set org.gnome.shell.extensions.tophat disk-display numeric
 gsettings set org.gnome.shell.extensions.tophat show-mem true
 gsettings set org.gnome.shell.extensions.tophat mem-display numeric
 gsettings set org.gnome.shell.extensions.tophat network-usage-unit bits
+
+# Install new extensions
+gext install openweather-extension@jenslody.de
