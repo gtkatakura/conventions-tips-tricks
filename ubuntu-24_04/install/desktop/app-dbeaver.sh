32f0a799472a1b84c67c286c5184aab2fb181015
gtkatakura <gt.katakura@gmail.com>
2025-02-07 16:16:40 -0300

    fix(ubuntu): configures dbeaver as system install

--- ubuntu-24_04/install/desktop/app-dbeaver.sh
+++ ubuntu-24_04/install/desktop/app-dbeaver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 green_text "Installing DBeaver (using flatpak)"
-flatpak install -y flathub io.dbeaver.DBeaverCommunity
+flatpak install flathub io.dbeaver.DBeaverCommunity -y --system
