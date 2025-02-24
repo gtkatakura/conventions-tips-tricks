c63f64dfc74c93a203f41b26329a3ea9f98f2e30
gtkatakura <gt.katakura@gmail.com>
2025-02-24 14:54:52 -0300

    fix(ubuntu): install or update on the awscli

--- ubuntu-24_04/install/terminal/app-awscli.sh
+++ ubuntu-24_04/install/terminal/app-awscli.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+TEMP_DIR=$(mktemp -d)
+
 green_text "Downloading AWS CLI (x86_64)"
-wget -P /tmp https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
+wget -P "$TEMP_DIR" https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
 
 green_text "Unzipping AWS CLI (x86_64)"
-unzip /tmp/awscli-exe-linux-x86_64.zip -d /tmp
+unzip "$TEMP_DIR"/awscli-exe-linux-x86_64.zip -d "$TEMP_DIR"
 
 green_text "Installing AWS CLI (x86_64)"
-sudo /tmp/aws/install
+sudo "$TEMP_DIR"/aws/install --update
