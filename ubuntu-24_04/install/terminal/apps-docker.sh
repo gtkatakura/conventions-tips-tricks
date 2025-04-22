075f8d5098f340a467b38794388ac9db33449349
gtkatakura <gt.katakura@gmail.com>
2025-04-22 15:24:41 -0300

    fix(ubuntu): do not try to create docker-hostmanager if it already exist

--- ubuntu-24_04/install/terminal/apps-docker.sh
+++ ubuntu-24_04/install/terminal/apps-docker.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo docker run -d --name docker-hostmanager --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /etc/hosts:/hosts iamluc/docker-hostmanager
+# Create docker-hostmanager container if it doesn't exist
+if ! sudo docker ps -a --format '{{.Names}}' | grep -q "^docker-hostmanager$"; then
+  sudo docker run -d \
+    --name docker-hostmanager \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /etc/hosts:/hosts \
+    iamluc/docker-hostmanager
+fi
