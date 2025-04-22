2435c4ae47d6f2ca89e004e1a3980704ba0574f4
gtkatakura <gt.katakura@gmail.com>
2025-04-22 15:16:16 -0300

    fix(ubuntu): use the download api to discover the latest version url

--- ubuntu-24_04/install/desktop/app-cursor.sh
+++ ubuntu-24_04/install/desktop/app-cursor.sh
@@ -9,7 +9,13 @@ blue_text "Configuring AppImage dependencies"
 sudo sysctl -w kernel.apparmor_restrict_unprivileged_userns=0
 
 green_text "Downloading Cursor (AppImage)"
-sudo curl -L https://downloader.cursor.sh/linux/appImage/x64 -o /opt/cursor.appimage
+cursor_download_link=$(
+  curl -s 'https://www.cursor.com/api/download?platform=linux-x64&releaseTrack=stable' \
+    -H 'content-type: application/json' \
+  | jq -r '.downloadUrl'
+)
+
+sudo curl -L "$cursor_download_link" -o /opt/cursor.appimage
 sudo chmod +x /opt/cursor.appimage
 
 green_text "Creating the desktop shortcut"
