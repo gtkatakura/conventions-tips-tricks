118f858d7cdae70ff82ab2c7e3f33690527853f1
gtkatakura <gt.katakura@gmail.com>
2025-08-05 00:12:55 -0300

    feat(ubuntu): crush install

---
+++ ubuntu-24_04/install/terminal/app-crush.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+blue_text "Adds GPG key (charm)"
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+
+blue_text "Setting up the repository (charm)"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" \
+  | sudo tee /etc/apt/sources.list.d/charm.list > /dev/null
+
+green_text "Installing crush"
+sudo apt update
+sudo apt install crush -y
