1ec56a61243dbf2ea159be9797974b633dd34859
gtkatakura <gt.katakura@gmail.com>
2025-08-09 20:19:00 -0300

    wip

---
+++ macbook/defaults/bash/android.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export ANDROID_HOME=$HOME/Library/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
