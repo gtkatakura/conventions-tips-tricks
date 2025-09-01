4d45282d37155389e172b33a1d7dee63801d796e
gtkatakura <gt.katakura@gmail.com>
2025-09-01 00:46:31 -0300

    feat(ubuntu): add sourcing of rc.sh in .bashrc for terminal setup

--- ubuntu-24_04/install/terminal.sh
+++ ubuntu-24_04/install/terminal.sh
@@ -3,5 +3,17 @@
 
 sudo apt update -y
 
+if ! grep -Fxq 'source ~/.local/share/takashi/ubuntu-24_04/defaults/bash/rc.sh' ~/.bashrc; then
+  # Add the source line after the omakub line in .bashrc
+  omakub_line=$(grep -n 'source ~/.local/share/omakub/' ~/.bashrc | cut -d: -f1 | head -n1)
+
+  if [ -n "$omakub_line" ]; then
+    sed -i "$((omakub_line+1))isource ~/.local/share/takashi/ubuntu-24_04/defaults/bash/rc.sh" ~/.bashrc
+  else
+    echo "Error: omakub not found in .bashrc"
+    exit 1
+  fi
+fi
+
 # Run terminal installers
 for installer in ~/.local/share/takashi/ubuntu-24_04/install/terminal/*.sh; do source "$installer"; done
