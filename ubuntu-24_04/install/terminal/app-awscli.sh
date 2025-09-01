1b3980c202b04b7419cf13cb86d1ede5664f33fd
gtkatakura <gt.katakura@gmail.com>
2025-09-01 00:15:12 -0300

    fix(ubuntu): suppress output during AWS CLI unzipping in installation script

--- ubuntu-24_04/install/terminal/app-awscli.sh
+++ ubuntu-24_04/install/terminal/app-awscli.sh
@@ -6,7 +6,7 @@ green_text "Downloading AWS CLI (x86_64)"
 wget -P "$TEMP_DIR" https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
 
 green_text "Unzipping AWS CLI (x86_64)"
-unzip "$TEMP_DIR"/awscli-exe-linux-x86_64.zip -d "$TEMP_DIR"
+unzip -q "$TEMP_DIR"/awscli-exe-linux-x86_64.zip -d "$TEMP_DIR"
 
 green_text "Installing AWS CLI (x86_64)"
 sudo "$TEMP_DIR"/aws/install --update
