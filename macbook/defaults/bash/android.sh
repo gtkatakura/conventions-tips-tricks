295faf656b0801676731834a0569ee9c16d68c2c
gtkatakura <gt.katakura@gmail.com>
2025-09-02 23:58:22 -0300

    fix(macbook): correct ANDROID_HOME path and enable sourcing in rc.sh

--- macbook/defaults/bash/android.sh
+++ macbook/defaults/bash/android.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ANDROID_HOME=$HOME/Library/Android/Sdk
+export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
--- macbook/defaults/bash/rc.sh
+++ macbook/defaults/bash/rc.sh
@@ -5,17 +5,5 @@ alias cd='z'
 
 source ~/.local/share/takashi/shared/defaults/bash/rc.sh
 
-# source ~/.local/share/takashi/macbook/defaults/bash/aliases.sh
-# source ~/.local/share/takashi/macbook/defaults/bash/keyboard.sh
-# source ~/.local/share/takashi/macbook/defaults/bash/prompt.sh
-# source ~/.local/share/takashi/macbook/defaults/bash/shell.sh
-
-# source ~/.local/share/takashi/macbook/defaults/bash/android.sh
+source ~/.local/share/takashi/macbook/defaults/bash/android.sh
 source ~/.local/share/takashi/macbook/defaults/bash/brew.sh
-
-# source ~/.local/share/takashi/macbook/defaults/bash/dotnet.sh
-
-# source ~/.local/share/takashi/macbook/defaults/bash/mac.sh
-
-# Fixes for AWS VPN Client
-export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
