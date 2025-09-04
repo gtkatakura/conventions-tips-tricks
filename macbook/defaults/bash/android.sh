16f450162ff4fd67262b41a71fb673b87a7b75d5
gtkatakura <gt.katakura@gmail.com>
2025-09-04 01:05:20 -0300

    fix(macbook): add conditional check for Android SDK directory before setting environment variables

--- macbook/defaults/bash/android.sh
+++ macbook/defaults/bash/android.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
+if [ -d "$HOME/Library/Android/sdk" ]; then
+  export ANDROID_HOME=$HOME/Library/Android/sdk
+  export PATH=$PATH:$ANDROID_HOME/emulator
+  export PATH=$PATH:$ANDROID_HOME/platform-tools
+  export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
+fi
