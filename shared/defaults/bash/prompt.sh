217bcc3c2d9937dfa5b45f42b0cf4d66eca580d2
gtkatakura <gt.katakura@gmail.com>
2025-09-04 01:07:00 -0300

    refactor(shared): streamline prompt.sh for improved zsh compatibility and remove redundant code

--- shared/defaults/bash/prompt.sh
+++ shared/defaults/bash/prompt.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-# Color codes compatible with both bash and zsh
-if [[ -n "$ZSH_VERSION" ]]; then
-  COLOR_OFF="%f"
-  GREEN="%F{green}"
-  YELLOW="%F{yellow}"
-else
-  COLOR_OFF="\[\033[0m\]"
-  GREEN="\[\033[0;32m\]"
-  YELLOW="\[\033[0;33m\]"
-fi
-
 parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
 }
 
 if [[ -n "$ZSH_VERSION" ]]; then
+  COLOR_OFF="%f"
+  GREEN="%F{green}"
+  YELLOW="%F{yellow}"
+
   setopt PROMPT_SUBST
   export PROMPT="${GREEN}%~ ${YELLOW}\$(parse_git_branch)${COLOR_OFF}$ "
 else
+  COLOR_OFF="\[\033[0m\]"
+  GREEN="\[\033[0;32m\]"
+  YELLOW="\[\033[0;33m\]"
+
   export PS1="${GREEN}\w ${YELLOW}\$(parse_git_branch)${COLOR_OFF}$ "
 fi
