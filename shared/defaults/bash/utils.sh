6c8f4f1813fd28973b438003dc69f32953ccfb32
gtkatakura <gt.katakura@gmail.com>
2025-09-05 20:32:53 -0300

    feat(shared): enhance color variable definitions for improved compatibility between bash and zsh

--- shared/defaults/bash/utils.sh
+++ shared/defaults/bash/utils.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-COLOR_OFF="\[\033[0m\]"
-GREEN="\[\033[0;32m\]"
-YELLOW="\[\033[0;33m\]"
-RED="\[\033[0;91m\]"
-BLUE="\[\033[0;34m\]"
-NO_COLOR="\[\033[0m\]"
+if [[ -n "$ZSH_VERSION" ]]; then
+  COLOR_OFF="%f"
+  GREEN="%F{green}"
+  YELLOW="%F{yellow}"
+  RED="%F{red}"
+  BLUE="%F{blue}"
+  NO_COLOR="%f"
+else
+  COLOR_OFF="\033[0m"
+  GREEN="\033[0;32m"
+  YELLOW="\033[0;33m"
+  RED="\033[0;91m"
+  BLUE="\033[0;34m"
+  NO_COLOR="\033[0m"
+fi
 
 blue_text() {
   echo -e "${BLUE}$*${NO_COLOR}"
